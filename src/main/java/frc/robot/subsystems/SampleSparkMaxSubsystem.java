bdd381c5066df47e0ed73ea6c4183e9a318021dd
Jayden <Loading3141@gmail.com>
2025-01-04 15:02:58 -0800

    Update to 2025 stable

--- src/main/java/frc/robot/subsystems/SampleSparkMaxSubsystem.java
+++ src/main/java/frc/robot/subsystems/SampleSparkMaxSubsystem.java
@@ -2,11 +2,11 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.ClosedLoopConfig.ClosedLoopSlot;
 import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
